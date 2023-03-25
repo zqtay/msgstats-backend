e312017b54a750d52f41561b41af9a1841e0a39c
zqtay <zqtay96@gmail.com>
2023-03-25 14:41:30 +0800

    Add echo api

--- dist/index.js
+++ dist/index.js
@@ -4,12 +4,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT;
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
 app.get('/', (req, res) => {
     res.send('Express + TypeScript Server');
+});
+app.post('/echo', (req, res) => {
+    res.send(req.body);
 });
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express, { Express, Request, Response } from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -6,8 +7,15 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT;
 
+app.use(cors());
+app.use(express.json());
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
+});
+
+app.post('/echo', (req: Request, res: Response) => {
+  res.send(req.body);
 });
 
 app.listen(port, () => {
