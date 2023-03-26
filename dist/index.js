98f34472a026227b2d86a938f8e011df780bae07
zqtay <zqtay96@gmail.com>
2023-03-26 17:52:18 +0800

    Add test API for db

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -6,17 +15,84 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const mongodb_1 = require("mongodb");
+// Env Var
 dotenv_1.default.config();
+const { MONGODB_URI, MONGODB_USERNAME, MONGODB_PASSWORD, MONGODB_DB, PORT } = process.env;
+// Mongo
+let dbUri = MONGODB_URI ? MONGODB_URI : "";
+if (MONGODB_USERNAME && MONGODB_PASSWORD) {
+    const dbUser = MONGODB_USERNAME;
+    const dbPw = MONGODB_PASSWORD;
+    dbUri = dbUri.replace("<username>", dbUser).replace("<password>", dbPw);
+}
+const client = new mongodb_1.MongoClient(dbUri, { serverApi: mongodb_1.ServerApiVersion.v1 });
+client.connect().then(() => {
+    console.log("Database connected!");
+}).catch(err => {
+    client.close();
+});
+const MSGDATA_COLLECTION_NAME = "msg_data";
+// Express
 const app = (0, express_1.default)();
-const port = process.env.PORT;
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.get('/', (req, res) => {
-    res.send('Express + TypeScript Server');
+    res.send('Express Server');
 });
 app.post('/echo', (req, res) => {
     res.send(req.body);
 });
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+app.get('/test', (req, res) => {
+    const collection = client.db(MONGODB_DB).collection(MSGDATA_COLLECTION_NAME);
+    collection.find({}).toArray().then(r => res.send(r));
+});
+app.post('/test', (req, res) => {
+    let body = req.body;
+    if (body) {
+        body = Object.assign(Object.assign({}, body), { "_dateCreated": (new Date()).toISOString() });
+        const collection = client.db(MONGODB_DB).collection(MSGDATA_COLLECTION_NAME);
+        collection.insertOne(body).then(r => {
+            res.status(201);
+            res.send({
+                "insertedId": r.insertedId.toHexString()
+            });
+        });
+    }
+    else {
+        res.status(500);
+        res.send("Error");
+    }
+});
+app.get('/test/:id', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const collection = client.db(MONGODB_DB).collection(MSGDATA_COLLECTION_NAME);
+    const id = new mongodb_1.ObjectId(req.params.id);
+    const r = yield collection.findOne({ "_id": id });
+    if (r) {
+        res.send(r);
+    }
+    else {
+        res.status(404);
+        res.send("Not found");
+    }
+}));
+app.delete('/test/:id', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const collection = client.db(MONGODB_DB).collection(MSGDATA_COLLECTION_NAME);
+    const id = new mongodb_1.ObjectId(req.params.id);
+    const r = yield collection.deleteOne({ "_id": id });
+    if (r.deletedCount === 0) {
+        res.status(404);
+        res.send("Not found");
+    }
+    else if (r.deletedCount === 1) {
+        res.status(204);
+        res.send("Deleted");
+    }
+    else {
+        res.status(500);
+        res.send("Error");
+    }
+}));
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,101 @@
 import express, { Express, Request, Response } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import { MongoClient, ObjectId, ServerApiVersion } from "mongodb";
 
+// Env Var
 dotenv.config();
+const { MONGODB_URI, MONGODB_USERNAME, MONGODB_PASSWORD, MONGODB_DB, PORT } = process.env;
 
+// Mongo
+let dbUri = MONGODB_URI ? MONGODB_URI : "";
+if (MONGODB_USERNAME && MONGODB_PASSWORD) {
+  const dbUser = MONGODB_USERNAME;
+  const dbPw = MONGODB_PASSWORD;
+  dbUri = dbUri.replace("<username>", dbUser).replace("<password>", dbPw);
+}
+
+const client = new MongoClient(dbUri, { serverApi: ServerApiVersion.v1 });
+client.connect().then(
+  () => {
+    console.log("Database connected!");
+  }
+).catch(err => {
+  client.close();
+});
+const MSGDATA_COLLECTION_NAME = "msg_data";
+
+// Express
 const app: Express = express();
-const port = process.env.PORT;
-
 app.use(cors());
 app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
+  res.send('Express Server');
 });
 
 app.post('/echo', (req: Request, res: Response) => {
   res.send(req.body);
 });
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+app.get('/test', (req: Request, res: Response) => {
+  const collection = client.db(MONGODB_DB).collection(MSGDATA_COLLECTION_NAME);
+  collection.find({}).toArray().then(
+    r => res.send(r)
+  );
+});
+
+app.post('/test', (req: Request, res: Response) => {
+  let body = req.body;
+  if (body) {
+    body = {...body, "_dateCreated": (new Date()).toISOString()};
+    const collection = client.db(MONGODB_DB).collection(MSGDATA_COLLECTION_NAME);
+    collection.insertOne(body).then(
+      r => {
+        res.status(201);
+        res.send({
+          "insertedId": r.insertedId.toHexString()
+        });
+      }
+    );
+  }
+  else {
+    res.status(500);
+    res.send("Error");
+  }
+});
+
+app.get('/test/:id', async (req: Request, res: Response) => {
+  const collection = client.db(MONGODB_DB).collection(MSGDATA_COLLECTION_NAME);
+  const id = new ObjectId(req.params.id);
+  const r = await collection.findOne({ "_id": id });
+  if (r) {
+    res.send(r);
+  }
+  else {
+    res.status(404);
+    res.send("Not found");
+  }
+});
+
+app.delete('/test/:id', async (req: Request, res: Response) => {
+  const collection = client.db(MONGODB_DB).collection(MSGDATA_COLLECTION_NAME);
+  const id = new ObjectId(req.params.id);
+  const r = await collection.deleteOne({ "_id": id });
+  if (r.deletedCount === 0) {
+    res.status(404);
+    res.send("Not found");
+  }
+  else if (r.deletedCount === 1) {
+    res.status(204);
+    res.send("Deleted");
+  }
+  else {
+    res.status(500);
+    res.send("Error");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
